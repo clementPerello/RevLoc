6d474f9782ed16e08db360ff2be5b57f7212c15c
Clément <clement.perello.sio@gmail.com>
2015-11-23 15:26:40 +0100

    Création de la classe principale de l'application (avec la présence de la méthode main)

---
+++ RevLoc2/src/fr/adavis/reLoc/Garagiste.java
@@ -0,0 +1,5 @@
+package fr.adavis.reLoc;
+
+public class Garagiste {
+
+}
---
+++ RevLoc2/src/fr/adavis/reLoc/MainApp.java
@@ -0,0 +1,10 @@
+package fr.adavis.reLoc;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
